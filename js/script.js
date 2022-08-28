207a0d5e14e46718c8462d9d7546e16e1e0020bb
vodnibivol <vodnibivol@gmail.com>
2022-08-28 11:26:09 +0200

    update

--- js/script.js
+++ js/script.js
@@ -7,23 +7,27 @@ class Element {
 }
 
 const Unhook = (function () {
+  // vars
   const $ = (sel) => document.querySelector(sel);
   // prettier-ignore
   const STYLE_ID = "style_" + new Array(8).fill().map((_) => Math.floor(Math.random() * 16).toString(16)).join('');
   const STYLE_EL = document.createElement('style');
   STYLE_EL.setAttribute('id', STYLE_ID);
 
+  const STORAGE_KEY = 'UNHOOK_KEY';
+
   const S = [
     new Element(".grid-disabled[page-subtype='home']", 'home screen'),
     new Element('#related', 'related'),
     new Element('#comments', 'comments'),
     new Element('#chat', 'chat'),
-    // new Element('#comment-teaser', 'comment teaser'),
-    // new Element('.html5-endscreen', 'endscreen'),
-    // new Element('#primary.ytd-watch-flexy', 'center', '{position:relative;left:49%;transform:translateX(-50%);}'),
-    // new Element('#ytd-guide-signin-promo-renderer,a[href*="Login"]', 'login promo'),
+    new Element('#comment-teaser', 'comment teaser'),
+    new Element('.html5-endscreen', 'endscreen'),
+    new Element('#primary.ytd-watch-flexy', 'center', '{position:relative;left:49%;transform:translateX(-50%);}'),
+    new Element('#ytd-guide-signin-promo-renderer,a[href*="Login"]', 'login promo'),
   ];
 
+  // events
   $('#yt-unhook-btn').addEventListener('click', () => $('#yt-unhook-menu').classList.remove('unhook-hidden'));
   $('#yt-unhook-menu').addEventListener('click', function (e) {
     if (e.target === this) this.classList.add('unhook-hidden');
@@ -35,6 +39,14 @@ const Unhook = (function () {
   $('#yt-unhook-menu form').onchange = _formChange;
 
   _inject();
+  $('#yt-unhook-menu').classList.add('unhook-hidden'); // hide menu by default
+
+  // load localStorage
+  const savedStyles = localStorage.getItem(STORAGE_KEY);
+  if (savedStyles) {
+    let arr = JSON.parse(savedStyles);
+    [...$('#yt-unhook-menu form').querySelectorAll('input')].forEach((i) => (i.checked = arr.includes(i.name)));
+  }
 
   // f(x)
   function _inject() {
@@ -49,6 +61,7 @@ const Unhook = (function () {
 
   function _formChange() {
     let checked = [...$('#yt-unhook-menu form').querySelectorAll('input')].filter((e) => e.checked).map((e) => e.name);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(checked)); // set to localStorage. FMT: ["...", "..."]
     let filtered = S.filter((e) => checked.includes(e.name));
     let style = filtered.map((e) => e.selector + e.style).join(' ');
     _setStyle(style);
